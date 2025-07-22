45d6890c369027252d7406505897178e4fe1a440
Anagha <anaghabharadwaj621@gmail.com>
2025-07-22 21:52:05 +0530

    Created header component

--- auth-app/src/App.jsx
+++ auth-app/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./components/About";
-import Home from "./components/Home.jsx";
-import Profile from "./components/Profile.jsx";
-import SignIn from "./components/SignIn.jsx";
-import SignUp from "./components/SignUp.jsx";
+import About from "./Pages/About.jsx";
+import Home from "./Pages/Home.jsx";
+import Profile from "./Pages/Profile.jsx";
+import SignIn from "./Pages/SignIn.jsx";
+import SignUp from "./Pages/SignUp.jsx";
+import Nav from "./components/Nav.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+    {/* Header component should be all pages that's why it is here */}
+    <Nav/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
