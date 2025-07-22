c4801361de4dca6a08fbecac1cf958b78a76cbc5
Anagha <anaghabharadwaj621@gmail.com>
2025-07-22 21:36:44 +0530

    Added routes and created pages

--- auth-app/src/App.jsx
+++ auth-app/src/App.jsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home.jsx";
+import Profile from "./components/Profile.jsx";
+import SignIn from "./components/SignIn.jsx";
+import SignUp from "./components/SignUp.jsx";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
