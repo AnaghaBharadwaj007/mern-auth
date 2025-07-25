3d3ab9a5b52143bcf3b3867b79d4d07640ee25df
Anagha <anaghabharadwaj621@gmail.com>
2025-07-25 21:31:23 +0530

    Created Signin api route, Tokens and Cookie

--- apis/controllers/auth.controler.js
+++ apis/controllers/auth.controler.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../Models/User.model.js";
+import { errorHandler } from "../error.js";
 
 export const signup = async (req, res, next) => {
   // console.log(req.body);
@@ -8,6 +10,7 @@ export const signup = async (req, res, next) => {
     const hashedPassword = bcrypt.hashSync(password, 10);
     await User.create({ username, email, password: hashedPassword });
     return res.status(200).json("User created Successfully");
+
   }
   catch (err) {
     console.log(err)
@@ -15,3 +18,25 @@ export const signup = async (req, res, next) => {
   }
 }
 //User.create() is shorthand for creating + saving in one step.
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User with that email not found"))
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+
+    //separating the password from rest , to send it to use as response.
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    //creating a token {payload, secretOrPrivateKey, [options]}
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+  }
+  catch (err) {
+    next(err);
+  }
+
+}
--- apis/routes/auth-route.js
+++ apis/routes/auth-route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controler.js";
+import { signin, signup } from "../controllers/auth.controler.js";
 
 const router = express.Router();
 
-router.post("/sign-up", signup)
+router.post("/sign-up", signup);
+router.post("/sign-in", signin);
 
 export default router;
